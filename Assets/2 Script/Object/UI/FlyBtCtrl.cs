5930e7e267812754c263a6b93d1f8feb3c11ad6b
Irang Kim <myo_con@naver.com>
2016-07-12 02:38:10 +0900

    UI - FlyButton Drag 추가 ( 추가작업 필요 )

--- Assets/2 Script/Object/UI/FlyBtCtrl.cs
+++ Assets/2 Script/Object/UI/FlyBtCtrl.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class FlyBtCtrl : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler 
 {
-    public Transform tr;
-    public Transform boostTr;
-    private Vector3 StartPosition;
+    private Transform tr;
+    public Transform StartTr;  // 처음 시작하는 위치 ( FlyButton위치 )           
+    public Transform endTr;       // 부스트의 위치
+    private Vector3 startPosition;
+    private Vector3 endPosition;
     private bool isMouseDown = false;
+    private float fStraightAngle;
     
     private PlayerCtrl player;
     // public static 
@@ -15,34 +19,58 @@ public class FlyBtCtrl : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoin
     {
         tr = GetComponent<Transform>();
         player = GameObject.Find("Player").GetComponent<PlayerCtrl>();
+
+
+
+        // StartPosition = tr.InverseTransformPoint(tr.position); //StartTr.InverseTransformPoint(StartTr.position);
+
+        //endPosition = endTr.InverseTransformPoint(endTr.position);
+
+        //fStraightAngle = (StartPosition.y - endPosition.y) / (StartPosition.x - endPosition.x); // 직선의방정식 기울기 
     }
 
-    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+ 
+   void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
     {
         isMouseDown = true;
-        StartPosition = tr.position;
-        
-        print("눌렀다 " + StartPosition);
+
+        startPosition = tr.position;
+        endPosition = endTr.position;
+
+        fStraightAngle = (endPosition.y - startPosition.y) / (endPosition.x - startPosition.x); // 직선의방정식 기울기 
+
         player.FlyBtDown();
     }
 
     void IDragHandler.OnDrag(PointerEventData eventData)
-    {
+    { 
         if (EventSystem.current.IsPointerOverGameObject())
         {
-            print("드래그한다");
-            tr.position = Input.mousePosition;
+            tr.position = CalculatePositionBetweenStartPositionAndBoostPosition(Input.mousePosition.x);//Input.mousePosition;////
             player.boostdown();
         }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        isMouseDown = false; 
-        tr.position = StartPosition;
-        print("뗐다 " + StartPosition);
+        isMouseDown = false;
+        tr.position = startPosition;
 
         player.FlyBtUp();
         player.boostup();
     }
+
+    private Vector3 CalculatePositionBetweenStartPositionAndBoostPosition(float _fX) // 마우스의 x값을 넣어서 y값을 정해주는 함수 
+    {
+       
+        
+        if (_fX < startPosition.x) _fX = startPosition.x;
+        if (_fX > endPosition.x) _fX = endPosition.x;
+
+       
+        float _fY = fStraightAngle * (_fX - startPosition.x) + startPosition.y;
+        print("여기까지" + new Vector3(_fX, _fY, 0f));
+        return new Vector3(_fX,_fY, 0f);
+    }
+
 }
