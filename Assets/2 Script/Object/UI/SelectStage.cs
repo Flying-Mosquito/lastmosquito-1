dae255c45f78da5578efd2d13b53a61950ed1421
rezzad <rezzad1@naver.com>
2016-07-28 19:39:49 +0900

    dd
    zzsdsad

---
+++ Assets/2 Script/Object/UI/SelectStage.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectStage : MonoBehaviour
+{
+    public Button exit;
+    public Button back;
+    // Use this for initialization
+
+
+    public bool backBool;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+   
+    public void Stage1()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void Stage2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
